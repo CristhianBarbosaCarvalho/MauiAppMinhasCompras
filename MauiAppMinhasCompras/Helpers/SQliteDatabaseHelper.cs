306d2bbe5f9638f58ffe7048ae018d7438bf15d8
CristhianBarbosaCarvalho <crizin9@gmail.com>
2025-08-18 00:54:12 -0300

    Construção da classe para fazer o Crud no SQlite

--- MauiAppMinhasCompras/Helpers/SQliteDatabaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQliteDatabaseHelper.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MauiAppMinhasCompras.Models;
+using SQLite;
 
 namespace MauiAppMinhasCompras.Helpers
 {
-    internal class SQliteDatabaseHelper
+    public class SQliteDatabaseHelper
     {
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQliteDatabaseHelper(string path) 
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+
+        public Task<int> Insert(Produto p) 
+        {
+            return _conn.InsertAsync(p);
+        }
+
+        public Task<int> Update(Produto p) 
+        {
+            return _conn.UpdateAsync(p);
+        }
+
+        public Task<int> Delete(int id) 
+        {
+            return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
+        }
+
+        public Task<List<Produto>> GetAll() 
+        {
+           return _conn.Table<Produto>().ToListAsync();
+        }
+
+        public Task<List<Produto>> Search(string q) 
+        {
+            string sql = "SELECT * FROM  WHERE Descricao LIKE '%" + q + "%'";
+
+            return _conn.QueryAsync<Produto>(sql);
+        }
+
     }
 }
